836371522f52d79dffb3274c3f062df23f586ac3
Sean D Gillespie <sean@mistersg.net>
2022-05-06 21:37:39 -0400

    Update resolver@lts-18.18

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{
+  ghc
+}:
+
+let
+  rev = "249c76eb6787420b2178a3ba4cc64c4d9c4a5997"; # pin
+  sha256 = "sha256:1lq2py428bfs6vv7qikzgh7hqyxw17c65dnfi35m46kjnidrrcbh";
+  pkgsArchive = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = sha256;
+  };
+
+  nixpkgs = import pkgsArchive {};
+  inherit (nixpkgs) haskell;
+in
+
+haskell.lib.buildStackProject {
+  inherit ghc;
+  name = "myEnv";
+  buildInputs = [];
+}
