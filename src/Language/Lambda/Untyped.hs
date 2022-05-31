e1cced875a6c09df95f35412478383eed57b3f69
Sean D Gillespie <sean@mistersg.net>
2022-05-30 21:56:56 -0400

    Add convenience functions for evaluating Strings

--- src/Language/Lambda/Untyped.hs
+++ src/Language/Lambda/Untyped.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Language.Lambda.Untyped (
   evalString,
+  runEvalString,
+  execEvalString,
+  unsafeExecEvalString,
   defaultUniques,
 
   module Language.Lambda.Untyped.Expression,
@@ -24,6 +27,28 @@ evalString :: String -> Eval String (LambdaExpr String)
 evalString = either throwParseError evalExpr . parseExpr
   where throwParseError = throwError . ParseError . pack . show
 
+runEvalString
+  :: String
+  -> Globals String
+  -> Either LambdaException (LambdaExpr String, EvalState String)
+runEvalString input globals' = runEval (evalString input) (mkState globals')
+
+execEvalString
+  :: String
+  -> Globals String
+  -> Either LambdaException (LambdaExpr String)
+execEvalString input globals' = execEval (evalString input) (mkState globals')
+
+unsafeExecEvalString
+  :: String
+  -> Globals String
+  -> LambdaExpr String
+unsafeExecEvalString input globals'
+  = unsafeExecEval (evalString input) (mkState globals')
+
 defaultUniques :: [String]
 defaultUniques = concatMap (\p -> map (:p) . reverse $ ['a'..'z']) suffix
   where suffix = "" : map show [(0::Int)..]
+
+mkState :: Globals String -> EvalState String
+mkState = flip EvalState defaultUniques
--- test/Language/Lambda/UntypedSpec.hs
+++ test/Language/Lambda/UntypedSpec.hs
@@ -1,5 +1,6 @@
 module Language.Lambda.UntypedSpec where
 
+import qualified RIO.Map as Map
 import Test.Hspec
 
 import Language.Lambda.Untyped
@@ -18,6 +19,48 @@ spec = do
 
     it "reduces applications with nested redexes" $
       eval "(\\f x. f x) (\\y. y)" `shouldBe` Right (Abs "x" (Var "x"))
+
+  describe "runEvalString" $ do
+    let runEvalString' input = fst <$> runEvalString input Map.empty
+    
+    it "evaluates simple strings" $ do
+      runEvalString' "x" `shouldBe` Right (Var "x")
+      runEvalString' "\\x. x" `shouldBe` Right (Abs "x" (Var "x"))
+      runEvalString' "f y" `shouldBe` Right (App (Var "f") (Var "y"))
+
+    it "reduces simple applications" $
+      runEvalString' "(\\x .x) y" `shouldBe` Right (Var "y")
+
+    it "reduces applications with nested redexes" $
+      runEvalString' "(\\f x. f x) (\\y. y)" `shouldBe` Right (Abs "x" (Var "x"))
+
+  describe "execEvalString" $ do
+    let execEvalString' input = execEvalString input Map.empty
+    
+    it "evaluates simple strings" $ do
+      execEvalString' "x" `shouldBe` Right (Var "x")
+      execEvalString' "\\x. x" `shouldBe` Right (Abs "x" (Var "x"))
+      execEvalString' "f y" `shouldBe` Right (App (Var "f") (Var "y"))
+
+    it "reduces simple applications" $
+      execEvalString' "(\\x .x) y" `shouldBe` Right (Var "y")
+
+    it "reduces applications with nested redexes" $
+      execEvalString' "(\\f x. f x) (\\y. y)" `shouldBe` Right (Abs "x" (Var "x"))
+
+  describe "unsafeExecEvalString" $ do
+    let unsafeExecEvalString' input = unsafeExecEvalString input Map.empty
+    
+    it "evaluates simple strings" $ do
+      unsafeExecEvalString' "x" `shouldBe` Var "x"
+      unsafeExecEvalString' "\\x. x" `shouldBe` Abs "x" (Var "x")
+      unsafeExecEvalString' "f y" `shouldBe` App (Var "f") (Var "y")
+
+    it "reduces simple applications" $
+      unsafeExecEvalString' "(\\x .x) y" `shouldBe` Var "y"
+
+    it "reduces applications with nested redexes" $
+      unsafeExecEvalString' "(\\f x. f x) (\\y. y)" `shouldBe` Abs "x" (Var "x")
 
   describe "defaultUniques" $ do
     let alphabet = reverse ['a'..'z']
